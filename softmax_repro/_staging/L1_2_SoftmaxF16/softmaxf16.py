c388da0dbb28277d1b891d6552840048ac70f581
BryanChen408 <chenbaixuan@huawei.com>
2026-06-05 09:33:20 +0000

    feat(softmax): add telemetry and source for L1_2_SoftmaxF16

---
+++ softmax_repro/_staging/L1_2_SoftmaxF16/softmaxf16.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return torch.softmax(x, dim=-1)
+
+
+def get_init_inputs():
+    return []
+
+
+def get_input_groups():
+    return [
+        [torch.randn([1024, 1024], dtype=torch.float16)],
+        [torch.randn([512, 768], dtype=torch.float16)],
+        [torch.randn([128, 2048], dtype=torch.float16)],
+    ]
